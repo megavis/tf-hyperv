9cae595854339b29764c13eac05bc84a6f750a96
megavis <ivanvernichenko@gmail.com>
2020-12-30 02:40:24 +0200

    instances improvements

---
+++ winrm/enable-winrm.ps1
@@ -0,0 +1,10 @@
+﻿Enable-PSRemoting -SkipNetworkProfileCheck -Force
+
+Set-WSManInstance WinRM/Config/WinRS -ValueSet @{MaxMemoryPerShellMB = 1024}
+Set-WSManInstance WinRM/Config -ValueSet @{MaxTimeoutms=1800000}
+Set-WSManInstance WinRM/Config/Client -ValueSet @{TrustedHosts="*"}
+Set-WSManInstance WinRM/Config/Service/Auth -ValueSet @{Negotiate = $true}
+
+# enable hyper-v tools
+#Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+Enable-WindowsOptionalFeature -Online -FeatureName  Microsoft-Hyper-V-Management-PowerShell
