cd2e65bf812328329fdd73d6733e862b36192c19
megavis <ivanvernichenko@gmail.com>
2020-12-30 02:43:11 +0200

    instances improvements

--- instances.tf
+++ instances.tf
@@ -1,34 +1,40 @@
 variable "master_nodes" {
-  default = 1
-}
-
-variable "data_nodes" {
   default = {
-    data_1 = "uahqsrv-elkd01"
+    master_1 = "uahqsrv-elkm01",
+    master_2 = "uahqsrv-elkm02",
   }
 }
 
-resource "hyperv_network_switch" "virtual_switch" {
-  name = "virtual_switch"
-  allow_management_os = true
+resource "hyperv_vhd" "main_vhd" {
+  provider = hyperv.main_stage
+
+  for_each = var.master_nodes
+
+  path = "D:\\Hyper-V\\test\\${each.value}.vhdx"
+  source_vm = "Debian"
 }
 
-#resource "hyperv_vhd" "master_vhd" {
-#  for_each = var.master_nodes
-#  name =
-#}
-resource "hyperv_machine_instance" "master_node" {
-  count = var.master_nodes
-  name = "uahqsrv-elkm${count.index}"
+resource "hyperv_network_switch" "vm_switch" {
+  provider = hyperv.main_stage
+
+  name = "vm_switch"
+}
+
+resource "hyperv_machine_instance" "default" {
+  provider = hyperv.main_stage
+
+  for_each = var.master_nodes
+
+  name = each.value
 
   network_adaptors {
     name = "wan"
-    switch_name = hyperv_network_switch.virtual_switch.name
+    switch_name = hyperv_network_switch.vm_switch.name
   }
 
   hard_disk_drives {
-    path = "d:\\Hyper-V\\test\\master-node-${count.index}.vhd"
-    controller_number = "0"
-    controller_location = "0"
+    path = "D:/Hyper-V/test/${each.value}.vhdx"
+    controller_number = 0
+    controller_location = 0
   }
 }
--- providers.tf
+++ providers.tf
@@ -6,5 +6,19 @@ terraform {
   }
 }
 
-provider "hyperv" {
+variable "admin_password" {
+  default = ""
 }
+
+provider "hyperv" {
+  alias = "main_stage"
+  user = "hyperv"
+  host = "127.0.0.1"
+  port = "5985"
+  https = false
+  use_ntlm = true
+}
+
+//provider "hyperv" {
+//  alias = "bcp_stage"
+//}
